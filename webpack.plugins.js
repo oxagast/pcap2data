c68a82d48ba27b9abe81a6cb9a0a1296ee065881
Marshall Whittaker <marshall@oxasploits.com>
2026-07-01 12:04:33 -0400

    Trying to fix the windows no style/js runtime bug now.

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,6 +1,33 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+class FixForgeRendererPathsPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('FixForgeRendererPathsPlugin', (compilation) => {
+      const { Compilation, sources } = compiler.webpack;
+      compilation.hooks.processAssets.tap(
+        {
+          name: 'FixForgeRendererPathsPlugin',
+          stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE,
+        },
+        () => {
+          const htmlAssetName = 'main_window/index.html';
+          const htmlAsset = compilation.getAsset(htmlAssetName);
+          if (!htmlAsset) {
+            return;
+          }
+
+          let html = htmlAsset.source.source().toString();
+          html = html.replace(/src="\/main_window\//g, 'src="../main_window/');
+          html = html.replace(/href="assets\/css\/style\.css"/g, 'href="../assets/css/style.css"');
+
+          compilation.updateAsset(htmlAssetName, new sources.RawSource(html));
+        },
+      );
+    });
+  }
+}
+
 module.exports = [
   new CopyPlugin({
     patterns: [
@@ -10,4 +37,5 @@ module.exports = [
       },
     ],
   }),
+  new FixForgeRendererPathsPlugin(),
 ];
