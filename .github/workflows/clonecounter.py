ed621f6b2936f09cd1bd10f463f1823023b06749
Marshall Whittaker <marshall@oxasploits.com>
2026-09-04 09:35:29 -0400

    Refactor clone counter logic and improve summation

--- .github/workflows/clonecounter.py
+++ .github/workflows/clonecounter.py
@@ -5,11 +5,6 @@ with open('clone.json', 'r') as fh:
 
 with open('clone_before.json', 'r') as fh:
     before = json.load(fh)
-
-# Handle first run: if 'clones' key is missing or empty, seed from 'now'
-if not before.get('clones'):
-    before['clones'] = []
-
 timestamps = {before['clones'][i]['timestamp']: i for i in range(len(before['clones']))}
 
 latest = dict(before)
@@ -20,17 +15,18 @@ for i in range(len(now['clones'])):
     else:
         latest['clones'].append(now['clones'][i])
 
-latest['count'] = sum(int(x['count']) for x in latest['clones'])
-latest['uniques'] = sum(int(x['uniques']) for x in latest['clones'])
+
+latest['count'] = sum(map(lambda x: int(x['count']), latest['clones']))
+latest['uniques'] = sum(map(lambda x: int(x['uniques']), latest['clones']))
 
 if len(timestamps) > 100:
     remove_this = []
     clones = latest['clones']
     for i in range(len(timestamps) - 35):
-        clones[i]['timestamp'] = clones[i]['timestamp'][:7]
+        clones[i]['timestamp'] = clones[i]['timestamp'][:7] 
         if clones[i]['timestamp'] == clones[i+1]['timestamp'][:7]:
-            clones[i+1]['count'] += clones[i]['count']
-            clones[i+1]['uniques'] += clones[i]['uniques']
+            clones[i+1]['count'] +=  clones[i]['count']
+            clones[i+1]['uniques'] +=  clones[i]['uniques']
             remove_this.append(clones[i])
 
     for item in remove_this:
@@ -38,3 +34,5 @@ if len(timestamps) > 100:
 
 with open('clone.json', 'w', encoding='utf-8') as fh:
     json.dump(latest, fh, ensure_ascii=False, indent=4)
+
+
